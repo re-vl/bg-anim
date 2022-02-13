302260c16235fe97390dc746ff68f99294bb87ef
Vladimir <rechmed@yandex.ru>
2022-02-13 16:39:10 +0300

    12345

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 jQuery(document).ready(function () {
-   let num, offset, duration;
+   let last = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      offset,
+      duration;
 
    jQuery("#main .elementor-background-overlay").append(`
    <div class="area">
       <ul class="circles">
          <li><i class="fab fa-html5 fa-4x"></i></li>
-         <li><i class="fas fa-code fa-4x"></i></li>
+
+      </ul>
+   </div>
+   `);
+
+   /*          <li><i class="fas fa-code fa-4x"></i></li>
          <li><i class="fab fa-css3 fa-4x"></i></li>
          <li><i class="fab fa-js fa-4x"></i></li>
          <li><i class="fab fa-node-js fa-4x"></i></li>
@@ -13,18 +20,28 @@ jQuery(document).ready(function () {
          <li><i class="fab fa-github fa-4x"></i></li>
          <li><i class="fab fa-sass fa-4x"></i></li>
          <li><i class="fab fa-elementor fa-4x"></i></li>
-         <li><i class="fas fa-code-branch fa-4x"></i></li>
-      </ul>
-   </div>
-   `);
-
-   console.log(jQuery().new());
+         <li><i class="fas fa-code-branch fa-4x"></i></li> */
 
    setInterval(function () {
-      jQuery(".circles li").each(function () {
+      let now = new Date();
+
+      jQuery(".circles li").each(function (i) {
          offset = Math.floor(Math.random() * (95 - 5)) + 5;
-         duration = Math.round(Math.random() * 20 + 10);
-         jQuery(this).css({ left: `${offset}%`, "animation-duration": `${duration}s` });
+         duration = Math.round(Math.random() * 5000 + 1000);
+
+         if (
+            now - last[i] >=
+            jQuery(this)
+               .css("animation-duration")
+               .replace(/[^-\d\.]/g, "") *
+               1000
+         ) {
+            jQuery(this).css({
+               left: `${offset}%`,
+               "animation-duration": `${duration}ms`,
+            });
+            last[i] = now;
+         }
       });
-   }, 5000);
+   }, 100);
 });
